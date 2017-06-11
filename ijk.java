f4bc55dcda6840bf0a1fb6ed9c81aa4d9ea1375e
Covarthanan <covart1196@gmail.com>
2017-06-11 23:10:36 +0530

    Create ijk.java

---
+++ ijk.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*
+public class ijk
+{
+  public static void main(String arg[])
+  {
+    int i,j,k,x=0,y=0,z=0;
+    Scanner s=new Scanner(System.in);
+    int a[]=new int[];
+    int n=s.nextInt();
+    for(i=0;i<n;i++)
+    {
+     a[i]=s.nextInt();
+     }
+     for(i=0;i<n;i++)
+     {
+       for(j=0;j<n;j++)
+       {
+         for(k=0;k<n;k++)
+         {
+           if((a[i]+a[j])==a[k])
+           {
+             x=a[i];
+             y=a[j];
+             z=a[k];
+             break;
+           }
+         }
+        }
+      }
+      System.out.print("i="+x+" j="+y+" k="+z);
+  }
+}  
